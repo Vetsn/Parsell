727ad6c7296c0337847e8c21d91fdab26f5099c7
Vetsn <34178776+Vetsn@users.noreply.github.com>
2018-06-26 18:17:18 +0900

    Delete sh-merge.sh

--- working-sample/sh-merge.sh
+++
@@ -1,5 +0,0 @@
-
-# trims parameters into JSON for handling in python
-offset=`sed -n '/\/\/embedded_reaction/ {=}' Sim_cond.cnd `
-sed "$offset q" Sim_cond.cnd | cat - dump > x; mv x Sim_cond.cnd
-
